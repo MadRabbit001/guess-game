7909ff73450f3cac019a924f969c73b08ae69fb4
MadRabbit001 <101290160+MadRabbit001@users.noreply.github.com>
2023-01-14 01:49:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 'use strict';
+
 let num;
 let score;
+
+// select the button you want to simulate the click event on
+const myButton = document.querySelector('.check');
+
+// select the input you want to listen to
+const myInput = document.querySelector('.guess');
+
+// add the event listener
+myInput.addEventListener('keyup', function (event) {
+  if (event.key === 'Enter') {
+    // create a new click event
+    const clickEvent = new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+    });
+    // dispatch the click event on the button
+    myButton.dispatchEvent(clickEvent);
+  }
+});
+
+function mistake() {
+  document
+    .querySelector('body')
+    .setAttribute('style', 'background-color: red;');
+}
+
+function regular() {
+  document
+    .querySelector('body')
+    .setAttribute('style', 'background-color: none;');
+}
+
 function again() {
   document.querySelector('.again').textContent = 'Again!';
-  document.querySelector('.score').textContent = 20;
+  document.querySelector('.score').textContent = 10;
   document
     .querySelector('body')
     .setAttribute('style', 'background-color: none;');
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.number').setAttribute('style', 'width:15rem;');
-  num = Math.floor(Math.random() * 20) + 1;
-  score = 20;
+  num = Math.floor(Math.random() * 100) + 1;
+  score = 10;
   return num;
 }
 
@@ -28,22 +62,34 @@ function checkNum() {
     }
     document
       .querySelector('body')
-      .setAttribute('style', 'background-color: green;');
+      .setAttribute('style', 'background-color: none;');
     console.log('success');
     document.querySelector('.number').setAttribute('style', 'width:25rem;');
     document.querySelector('.number').textContent = num;
+    anime();
     return;
   } else if (check < num) {
-    document.querySelector('.message').textContent = 'LOW';
-
-    console.log('low');
+    document.querySelector('.message').textContent = 'TOO LOW';
+    setTimeout(mistake, 0);
+    setTimeout(regular, 250);
+    setTimeout(mistake, 500);
+    setTimeout(regular, 700);
   } else if (check > num) {
-    document.querySelector('.message').textContent = 'HIGH';
-
-    console.log('high');
+    document.querySelector('.message').textContent = 'TOO HIGH';
+    setTimeout(mistake, 0);
+    setTimeout(regular, 250);
+    setTimeout(mistake, 500);
+    setTimeout(regular, 700);
   } else {
     document.querySelector('.message').textContent = 'You Need to Start';
   }
-  score--;
+  if (!(check == num)) {
+    score--;
+  }
   document.querySelector('.score').textContent = score;
+  if (score == 0) {
+    document.querySelector('.message').textContent =
+      "you're a loser and you need to die💩";
+    return;
+  }
 }
